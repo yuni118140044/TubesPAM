ecc00df4621ba4f7f561c69c5a2ae43f0a7ce780
Daniel_Sipangkar <Daniel222mu@gmail.com>
2021-05-04 18:29:14 +0700

    FrontEnd Profil

--- DanusanApp/src/assets/images/index.js
+++ DanusanApp/src/assets/images/index.js
@@ -3,5 +3,17 @@ import SpashBackground from './SplashBackground.png'
 import ImageHeader from './Header.png'
 import ImageMakanan from './Makanan.png'
 import TombolTambah from './TombolTambah.png'
+import FotoProfil from './FotoProfil.png'
+import FotoGaris from './Garis.png'
+import FotoPanah from './Panah.png'
 
-export{Logo,SpashBackground,ImageHeader,ImageMakanan,TombolTambah}
+export{
+    Logo,
+    SpashBackground,
+    ImageHeader,
+    ImageMakanan,
+    TombolTambah,
+    FotoProfil,
+    FotoGaris,
+    FotoPanah
+}
--- DanusanApp/src/pages/Profil/index.js
+++ DanusanApp/src/pages/Profil/index.js
@@ -1,15 +1,57 @@
 import React from 'react'
-import { Dimensions, ImageBackground, StyleSheet, Text, View } from 'react-native'
-import {ImageHeader } from '../../assets'
+import { Dimensions, Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import {ImageHeader, FotoProfil, FotoPanah, FotoGaris } from '../../assets'
 
 const Profil = () => {
     return (
         <View style={styles.container}>
             <ImageBackground source={ImageHeader} style={styles.header}></ImageBackground>
             <View style={styles.body}>
-                <Text>
-                    Profil
-                </Text>
+                <View style={styles.profil}>
+                    <Image source={FotoProfil}></Image>
+                    <Text style={styles.namalengkap}>Nama Lengkap</Text>
+                    <View>
+                        <TouchableOpacity style={styles.gantiisiprofil}>
+                            <Image source={FotoPanah} style={{marginLeft: windowWidth-50, width:4, height: 10, marginTop: 25, marginBottom: 4}}></Image>
+                            <Image source={FotoGaris} style={{width: windowWidth-40}}></Image>
+                            <Text style={{fontSize: 14, bottom: 20, left: 5}}>Username</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.gantiisiprofil}>
+                            <Image source={FotoPanah} style={{marginLeft: windowWidth-50, width:4, height: 10, marginTop: 25, marginBottom: 4}}></Image>
+                            <Image source={FotoGaris} style={{width: windowWidth-40}}></Image>
+                            <Text style={{fontSize: 14, bottom: 20, left: 5}}>No. Telepon</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.gantiisiprofil}>
+                            <Image source={FotoPanah} style={{marginLeft: windowWidth-50, width:4, height: 10, marginTop: 25, marginBottom: 4}}></Image>
+                            <Image source={FotoGaris} style={{width: windowWidth-40}}></Image>
+                            <Text style={{fontSize: 14, bottom: 20, left: 5}}>Email</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.gantiisiprofil}>
+                            <Image source={FotoPanah} style={{marginLeft: windowWidth-50, width:4, height: 10, marginTop: 25, marginBottom: 4}}></Image>
+                            <Image source={FotoGaris} style={{width: windowWidth-40}}></Image>
+                            <Text style={{fontSize: 14, bottom: 20, left: 5}}>Alamat</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.gantiisiprofil}>
+                            <Image source={FotoPanah} style={{marginLeft: windowWidth-50, width:4, height: 10, marginTop: 25, marginBottom: 4}}></Image>
+                            <Image source={FotoGaris} style={{width: windowWidth-40}}></Image>
+                            <Text style={{fontSize: 14, bottom: 20, left: 5}}>Jenis Kelamin</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={styles.profilbawah}>
+                        <TouchableOpacity style={styles.gantiisiprofil}>
+                            <Image source={FotoPanah} style={{marginLeft: windowWidth-50, width:4, height: 10, marginTop: 25, marginBottom: 4}}></Image>
+                            <Image source={FotoGaris} style={{width: windowWidth-40}}></Image>
+                            <Text style={{fontSize: 14, bottom: 20, left: 5}}>Atur Password</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.gantiisiprofil}>
+                            <Image source={FotoPanah} style={{marginLeft: windowWidth-50, width:4, height: 10, marginTop: 25, marginBottom: 4}}></Image>
+                            <Image source={FotoGaris} style={{width: windowWidth-40}}></Image>
+                            <Text style={{fontSize: 14, bottom: 20, left: 5}}>Keluar</Text>
+                        </TouchableOpacity>
+                    </View>
+                    
+                </View>
             </View>
         </View>
     )
@@ -34,11 +76,35 @@ const styles = StyleSheet.create({
         marginBottom: 15
     },
     body:{
-        width: windowWidth,
+        width: windowWidth+2,
         height: windowHeight,
-        borderRadius: 20,
+        borderTopLeftRadius : 20,
+        borderTopRightRadius : 20,
         borderColor: '#138BFE',
         borderWidth: 1,
-        backgroundColor: 'white'
+        backgroundColor: 'white',
+        flex: 1,
+    },
+    teksheader:{
+        paddingLeft: 20,
+        paddingTop: 15,
+        color: '#696969',
+        fontWeight: 'bold',
+    },
+    profil:{
+        alignContent: 'center',
+        alignItems: 'center',
+        paddingTop: 20
+    },
+    namalengkap:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#696969',
+    },
+    gantiisiprofil:{
+        marginBottom: -25
+    },
+    profilbawah:{
+        paddingTop: 50
     }
 })
